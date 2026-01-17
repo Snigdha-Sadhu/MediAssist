7c3cb0ed544f32eaa743b13c2f4c5839592a6167
Snigdha-Sadhu <snigdhasadhu2005@gmail.com>
2026-01-17 09:16:14 +0530

    minimal change for tesing

--- server/server.js
+++ server/server.js
@@ -31,7 +31,9 @@ const PORT=process.env.PORT|| 5000;
 mongoose.connect(process.env.MONGO_URL) .then(()=>{
   console.log('mongodb connected');
   
-  app.listen(PORT,()=>console.log(`server running ${PORT}`))
+  app.listen(PORT, "0.0.0.0", () => {
+  console.log("server running on", PORT);
+});
 })
 .catch(err => console.error(err));
  
