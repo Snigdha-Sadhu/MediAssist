8bdea30ddcaefb4d1f9c08993c98ffc02b5aeb85
Snigdha-Sadhu <snigdhasadhu2005@gmail.com>
2026-04-23 13:00:43 +0530

    dns issue

--- server/server.js
+++ server/server.js
@@ -1,6 +1,9 @@
+
+import dns from "dns";
+dns.setDefaultResultOrder("ipv4first");
+dns.setServers(['8.8.8.8', '8.8.4.4', '1.1.1.1']);
 import dotenv from "dotenv";
 dotenv.config();
-
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
